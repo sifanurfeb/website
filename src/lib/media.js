958b74bd93ccb5708b6fea1f218c1ff5f7390dc7
Sifa Nur Febriani <85918527+sifanurfeb@users.noreply.github.com>
2026-05-01 08:05:30 +0000

    update

---
+++ src/lib/media.js
@@ -0,0 +1,34 @@
+export default {
+  name: 'media',
+  title: 'Media',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'alt',
+      title: 'Alt Text',
+      type: 'string',
+    },
+  ],
+};
